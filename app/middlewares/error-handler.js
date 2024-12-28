76ba2263f4c25bf8e090f25183571a44975cbd60
ifaamrillah <amrillah.work@gmail.com>
2024-12-28 10:07:49 +0700

    feat: add error handler & route not found

---
+++ app/middlewares/error-handler.js
@@ -0,0 +1,38 @@
+const { StatusCodes } = require("http-status-codes");
+
+const errorHandler = (err, req, res, next) => {
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wrong",
+  };
+
+  if (err.name === "ValidationError") {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field, please choose another value`;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  if (err.name === "CastError") {
+    customError.msg = `No item found with id ${err.value}`;
+    customError.statusCode = StatusCodes.NOT_FOUND;
+  }
+
+  return res.status(customError.statusCode).json({ msg: customError.msg });
+};
+
+const routeNotFound = (req, res) => {
+  return res.status(StatusCodes.NOT_FOUND).json({ msg: "Route not found" });
+};
+
+module.exports = {
+  errorHandler,
+  routeNotFound,
+};
